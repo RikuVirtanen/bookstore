61f89ef8561e447f26a8386e9927c3d77ab7aac8
Riku Virtanen <virtanen.riku91@gmail.com>
2021-09-30 13:22:32 +0300

    RESTful service implementation for bookController

--- src/main/java/hh/swd20/bookstore/web/BookController.java
+++ src/main/java/hh/swd20/bookstore/web/BookController.java
@@ -1,17 +1,23 @@
 package hh.swd20.bookstore.web;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import hh.swd20.bookstore.domain.Book;
 import hh.swd20.bookstore.domain.BookRepository;
 import hh.swd20.bookstore.domain.CategoryRepository;
 
+@CrossOrigin
 @Controller
 public class BookController {
 	
@@ -31,6 +37,18 @@ public class BookController {
 		model.addAttribute("books", bookRepository.findAll());
 		return "booklist";
 	}
+	
+	// RESTful service to get all books 
+	@RequestMapping(value="/books", method=RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest() {
+		return (List<Book>) bookRepository.findAll();
+	}
+	
+	// RESTful service to get book by id 
+		@RequestMapping(value="/books/{id}", method=RequestMethod.GET)
+		public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long bookId) {
+			return bookRepository.findById(bookId);
+		}
 	
 	@RequestMapping(value="/save", method=RequestMethod.POST)
 	public String saveBook(@ModelAttribute Book book) {
